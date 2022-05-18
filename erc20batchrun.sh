06807f42bb88251e29ccbc193105a65dc05e77f4
liuye <liuye5@live.com>
2022-05-18 17:22:27 +0800

    add batchrun shs

--- erc20batchrun.sh
+++ erc20batchrun.sh
@@ -1,5 +1,6 @@
 
-
+startaddress="0x2bbad4421e2979fc41acbd1cc8dff467b3c98062"
+flag=0
 for list in $(ls ./ERC20)
 do 
     echo $list 
@@ -7,7 +8,14 @@ do
     # echo $contracts
     for contract in ${contracts[@]}
     do
-        if [[ $contract == 0x* ]]
+        if [ $contract == $startaddress ]
+        then 
+            flag=$(($flag+1))
+            echo $flag 
+            echo $contract
+            continue 
+        fi 
+        if [ $flag != 0 ] && [[ $contract == 0x* ]]
         then 
             echo $contract 
             invcon  --eth_address $contract --workspace ./Experiment/ERC20  
