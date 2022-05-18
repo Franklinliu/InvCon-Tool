188350149afabb30d41a1a810ef63ed11a9f0925
liuye <liuye5@live.com>
2022-05-18 17:22:03 +0800

    add scan.sh

--- scan.sh
+++ scan.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
 # bookkeeping sum
-totalcontracts=$(ls ./Experiment/ERC20/ |wc -l)
+# totalcontracts=$(ls ./Experiment/ERC20/ |wc -l)
 
-find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -H -E '^orig\(this\..*\)\s==\ssum\(this\..*\[\]\.getValue\(\)\)' {} \;
-echo "found bookkeeping invariants for "
-find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -l -E '^orig\(this\..*\)\s==\ssum\(this\..*\[\]\.getValue\(\)\)' {} \;|sort|uniq|wc -l
-echo "out of $totalcontracts contracts!!"
-echo "**************************"
+# find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -H -E '^orig\(this\..*\)\s==\ssum\(this\..*\[\]\.getValue\(\)\)' {} \;
+# echo "found bookkeeping invariants for "
+# find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -l -E '^orig\(this\..*\)\s==\ssum\(this\..*\[\]\.getValue\(\)\)' {} \;|sort|uniq|wc -l
+# echo "out of $totalcontracts contracts!!"
+# echo "**************************"
 
-# transfer invariants
-find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -H -E "^(\-)?((0\.9|1\.0)[0-9]+\s\*\s)*orig\(.*\)\s[+|-]\s\s*((0\.9|1\.0)[0-9]+\s\*\s)*this.*\[.*\]\.getValue\(\)\s[+|-]\s\s*((0\.9|1\.0)[0-9]+\s\*\s)*orig\(this.*\[.*\]\.getValue\(\)\)\s==\s0" {} \;
+# # transfer invariants
+# find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -H -E "^(\-)?((0\.9|1\.0)[0-9]+\s\*\s)*orig\(.*\)\s[+|-]\s\s*((0\.9|1\.0)[0-9]+\s\*\s)*this.*\[.*\]\.getValue\(\)\s[+|-]\s\s*((0\.9|1\.0)[0-9]+\s\*\s)*orig\(this.*\[.*\]\.getValue\(\)\)\s==\s0" {} \;
 
-echo "found transfer invariants for "
-find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -l -E "^(\-)?((0\.9|1\.0)[0-9]+\s\*\s)*orig\(.*\)\s[+|-]\s\s*((0\.9|1\.0)[0-9]+\s\*\s)*this.*\[.*\]\.getValue\(\)\s[+|-]\s\s*((0\.9|1\.0)[0-9]+\s\*\s)*orig\(this.*\[.*\]\.getValue\(\)\)\s==\s0" {} \;|sort|uniq|wc -l
-echo "out of $totalcontracts contracts!!"
-echo "**************************"
+# echo "found transfer invariants for "
+# find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -l -E "^(\-)?((0\.9|1\.0)[0-9]+\s\*\s)*orig\(.*\)\s[+|-]\s\s*((0\.9|1\.0)[0-9]+\s\*\s)*this.*\[.*\]\.getValue\(\)\s[+|-]\s\s*((0\.9|1\.0)[0-9]+\s\*\s)*orig\(this.*\[.*\]\.getValue\(\)\)\s==\s0" {} \;|sort|uniq|wc -l
+# echo "out of $totalcontracts contracts!!"
+# echo "**************************"
 
-# approve invariants
-find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -H -E '^orig\(.*\)\s==\sthis\..*\[pair\(.*\)\]\.getSubValue\(\)' {} \;
+# # approve invariants
+# find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -H -E '^orig\(.*\)\s==\sthis\..*\[pair\(.*\)\]\.getSubValue\(\)' {} \;
 
-echo "found approve invariants for "
-find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -l -E '^orig\(.*\)\s==\sthis\..*\[pair\(.*\)\]\.getSubValue\(\)' {} \;|sort|uniq|wc -l
-echo "out of $totalcontracts contracts!!"
-echo "**************************"
+# echo "found approve invariants for "
+# find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -l -E '^orig\(.*\)\s==\sthis\..*\[pair\(.*\)\]\.getSubValue\(\)' {} \;|sort|uniq|wc -l
+# echo "out of $totalcontracts contracts!!"
+# echo "**************************"
 
-# transferFrom invariants
-find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -H -E '^-\s.*\s-\ssum\(.*\)\s+.*sum\(.*\).*==.*0' {} \;
+# # transferFrom invariants
+# find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -H -E '^-\s.*\s-\ssum\(.*\)\s+.*sum\(.*\).*==.*0' {} \;
 
-echo "found transferFrom invariants for "
-find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -H -E '^-\s.*\s-\ssum\(.*\)\s+.*sum\(.*\).*==.*0' {} \;|sort|uniq|wc -l
-echo "out of $totalcontracts contracts!!"
-echo "**************************"
+# echo "found transferFrom invariants for "
+# find ./Experiment/ERC20/ -type f -name "*.inv" -exec grep -H -E '^-\s.*\s-\ssum\(.*\)\s+.*sum\(.*\).*==.*0' {} \;|sort|uniq|wc -l
+# echo "out of $totalcontracts contracts!!"
+# echo "**************************"
+
+python3 createFinalResult.py ./Experiment/ERC20/ ./Experiment/FinalReportERC20/
